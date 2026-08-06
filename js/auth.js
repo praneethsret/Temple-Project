c150657b63116155f99cc4920f478d8de4cc2dea
praneethsret <127277785+praneethsret@users.noreply.github.com>
2026-08-06 09:48:17 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,65 +1,22 @@
-import { auth } from "./firebase.js";
-
-import {
-signInWithEmailAndPassword,
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-
+const loginBtn = document.getElementById("loginBtn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const loginBtn = document.getElementById("loginBtn");
 const message = document.getElementById("message");
 
-if(loginBtn){
+loginBtn.addEventListener("click", () => {
 
-loginBtn.addEventListener("click",()=>{
+    if (email.value.trim() === "" || password.value.trim() === "") {
+        message.style.color = "red";
+        message.innerHTML = "Please enter Email and Password";
+        return;
+    }
 
-const userEmail = email.value.trim();
-const userPassword = password.value.trim();
+    message.style.color = "green";
+    message.innerHTML = "Logging in...";
 
-if(userEmail==="" || userPassword===""){
-message.style.color="red";
-message.innerHTML="Please enter email and password";
-return;
-}
-
-message.style.color="blue";
-message.innerHTML="Please wait...";
-
-signInWithEmailAndPassword(auth,userEmail,userPassword)
-
-.then(()=>{
-
-message.style.color="green";
-message.innerHTML="Login Successful";
-
-setTimeout(()=>{
-window.location.href="dashboard.html";
-},1000);
-
-})
-
-.catch((error)=>{
-
-message.style.color="red";
-message.innerHTML=error.message;
-
-});
-
-});
-
-}
-
-onAuthStateChanged(auth,(user)=>{
-
-if(user){
-
-console.log("Logged In");
-
-}else{
-
-console.log("Not Logged In");
-
-}
+    setTimeout(() => {
+        localStorage.setItem("loggedIn", "true");
+        window.location.href = "dashboard.html";
+    }, 800);
 
 });
